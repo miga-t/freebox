34012168f5dae8eeeab2ac9eba952253da4dff17
John Doe <johndoe@example.com>
2023-04-03 10:02:16 +0900

    debug propety in [...nextauth].ts is modified.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -62,7 +62,7 @@ export default NextAuth({
 		signIn: "/auth"
 	},
 
-	debug: process.env.NODE_ENV === "ent",
+	debug: process.env.NODE_ENV === "development",
 	adapter: PrismaAdapter(prismadb),
 	session: {
 		strategy: "jwt"
